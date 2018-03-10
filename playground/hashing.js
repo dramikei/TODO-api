4f37b6d23ea3b6f02d44e481e65601724d05584f
Raghav Vashisht <raghav2011rv@gmail.com>
2018-03-10 23:41:05 +0530

    Hashed user passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,19 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 
+var password = '123abc!';
 
+// bcrypt.genSalt(10, (err,salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
+var hashedPassword = '$2a$10$KkTL89MKA4eSj1TvAxOSj.1o0.XjK/WEdkQPx5fMlTt0fQt5agxMm';
 
-var data = {
-  id: 10
-};
+bcrypt.compare(password,hashedPassword, (err, result) => {
+  console.log(result);
+});
 
-var token = jwt.sign(data, 'abc123')
-console.log(token);
-
-var decoded = jwt.verify(token, 'abc123');
-console.log('decoded', decoded);
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, 'abc123')
+// console.log(token);
+//
+// var decoded = jwt.verify(token, 'abc123');
+// console.log('decoded', decoded);
 // jwt.sign
 // jwt.verify
 
